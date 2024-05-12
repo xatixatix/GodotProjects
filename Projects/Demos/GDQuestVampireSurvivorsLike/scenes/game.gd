0e13e141f93727ddcd4261a081071049b1ff9e52
Attila Diós <xatixatix@gmail.com>
2024-05-12 15:49:29 +0200

    menu added

--- Projects/Demos/GDQuestVampireSurvivorsLike/scenes/game.gd
+++ Projects/Demos/GDQuestVampireSurvivorsLike/scenes/game.gd
@@ -3,8 +3,19 @@ extends Node2D
 @onready var path_follow_2d = %PathFollow2D
 @onready var player = $Player
 @onready var game_over = %GameOver
+@onready var menu = %Menu
 
 var enemies: Array[CharacterBody2D]
+
+func _on_ready():
+	self.visible = false
+	game_over.visible = false
+	get_tree().paused = true
+
+func _on_menu_start_game():
+	get_tree().paused = false
+	self.visible = true
+	game_over.visible = false
 
 func spawn_slime():
 	const SLIME = preload("res://characters/spawnables/slime.tscn")
@@ -18,18 +29,25 @@ func _on_slime_spawn_timer_timeout():
 	spawn_slime()
 
 func _on_player_health_depleted():
-	%GameOver.visible = true
+	game_over.visible = true
 	get_tree().paused = true
 	print("paused")
 
 func _on_retry_button_pressed():
 	print("pressed")
 	player.health = 100
-	%GameOver.visible = false
-	
+	game_over.visible = false
+	clear_enemies()
+	get_tree().paused = false
+
+func _on_menu_button_pressed():
+	player.health = 100
+	game_over.visible = false
+	menu.start_menu.visible = true
+	clear_enemies()
+
+func clear_enemies():
 	for enemy in enemies:
 		if enemy != null:
 			enemy.queue_free()
-	
 	enemies.clear()
-	get_tree().paused = false
