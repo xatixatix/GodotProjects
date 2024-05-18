1bbb2b227d0de7c8a946e1805f5086b92d707f14
Attila Diós <xatixatix@gmail.com>
2024-05-18 22:55:09 +0200

    enums, arrays, random, function call

--- Projects/Demos/BrackeysGDscriptTutorial/basic.gd
+++ Projects/Demos/BrackeysGDscriptTutorial/basic.gd
@@ -4,6 +4,10 @@ extends Node
 
 var health: int = 100
 
+enum Alignment { ALLY, NEUTRAL, ENEMY }
+
+@export var unit_aligment = Alignment.ALLY
+
 func _ready():
 	# This is a comment
 	print("Hello world") # Printing in the console
@@ -16,10 +20,66 @@ func _ready():
 	var position3D = Vector3(1,2,3)
 	position2D.x += 2 # (1,5) --> (3,5)
 	print(position2D)
+	
+	# Function call
+	print("What is 1 + 2: " + str(add(1, 2)))
+	
+	# Random
+	var roll = randf()
+	if roll <= 0.8:
+		print("Common item")
+	else:
+		print("Rare item")
+	
+	var character_height = randi_range(140, 200)
+	print("Your character is " + str(character_height) + " heigh.")
+	
+	# Arrays
+	var items: Array[String] = ["Potion", "Feather", "Hide"]
+	print(items[0])
+	items[0] = "New Potion"
+	print(items[0])
+	items.remove_at(1)
+	items.append("Sword")
+	for item in items:
+		print(item)
+	var glass := 0.0
+	while glass < 0.5:
+		glass += randf_range(0.0, 0.5)
+		print(glass)
+	
+	# Dictionary key-value
+	var players = {
+		"Crook":	{"Level": 1, "Health": 80}, 
+		"Villain": 	{"Level": 35, "Health": 150}, 
+		"Boss": 	{"Level": 100, "Health": 300}
+	}
+	players["Villain"] = 50
+	players["Dwayne"] = 999
+	for username in players:
+		print(username + ": " + str(players[username]))
+	
+	# Enums
+	if unit_aligment == Alignment.ALLY:
+		print("Welcome")
+	else:
+		print("Not welcome")
+	match unit_aligment:
+		Alignment.ALLY: 
+			print("HAHA")
+		Alignment.NEUTRAL:
+			print("HEHE")
+		Alignment.ENEMY:
+			print("HOHO")
+		_:
+			print("WHO ARE YOU?!")
 
 # Just a function
 func shoot():
 	print("Shoot")
+
+func add(num1: int, num2: int) -> int:
+	return num1 + num2
 
 func _on_timer_timeout():
 	if label.text == "Hello":
